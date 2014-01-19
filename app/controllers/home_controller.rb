bc9034a215124db1202f761852c3fc39dd5d0fd1
Rob Esris <robesris@gmail.com>
2014-01-19 18:48:58 -0500

    Update search action to use limits and flickr client

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,20 @@
 class HomeController < ApplicationController
   def index
-    @tweets ||= []
+    @hashtag_images ||= []
   end
 
   def search
-    client = TwitterClient.new
-    @hashtags = client.recent_hashtags(params[:twitter_handle])
+    twitter_client = TwitterClient.new
+    hashtags = twitter_client.recent_hashtags(params[:twitter_handle])
+
+    limit = params[:limit].to_i
+    limit = 1 if limit < 1
+
+    flickr_client = FlickrClient.new(limit)
+    @hashtag_images = hashtags.map do |hashtag|
+      { hashtag: hashtag, image_urls: flickr_client.tagged_image_urls(hashtag) }
+    end
 
     render action: :index
   end
-
-
 end
