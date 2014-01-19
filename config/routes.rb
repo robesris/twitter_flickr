1392721ff33e9fa1395dfa14b52a898cfef174fc
Rob Esris <robesris@gmail.com>
2014-01-19 15:00:48 -0500

    Renamed erb template and added route and form for submitted twitter handle

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ TwitterFlickr::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get 'search' => 'home#search'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
