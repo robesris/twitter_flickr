4fe4603ee1c4dd15a72804e0f2f00b6d3762767b
Rob Esris <robesris@gmail.com>
2014-01-19 14:21:42 -0500

    Initialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterFlickr::Application.config.secret_key_base = '0fd667a19e0c563f31aad970fb3dc34ad750c10168a1ffaeef2746ca080a91e601b55f2ca9dbb2ac08aab4d817cf6cb348b0c7fff640efad621499920386af4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterFlickr::Application.config.session_store :cookie_store, key: '_twitter_flickr_session'
